9ea898b22b5793906307da8d629e156d8c5eb546
brianomad <brianomad@gmail.com>
2026-07-31 00:54:23 +0800

    fix: update build config for Vercel deployment compatibility

--- next.config.js
+++ next.config.js
@@ -1,26 +1,8 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
-const webpack = require('webpack')
-
-// const nextConfiguration = {
-//   target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
-// };
-
-// module.exports = withPlugins([optimizedImages], nextConfiguration);
 
 const nextConfiguration = {
-  target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
+  target: 'serverless',
 };
-const { parsed: myEnv } = require('dotenv').config({
-  path: '/full/custom/path/to/env'
-})
 
-
-module.exports =
-  withPlugins([optimizedImages], nextConfiguration),
-{
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
-    return config
-  }
-}
+module.exports = withPlugins([optimizedImages], nextConfiguration);
