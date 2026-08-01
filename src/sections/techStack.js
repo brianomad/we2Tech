a17c806d0d500978e709db4faa1758a14b5cb5b0
brianomad <brianomad@gmail.com>
2026-08-01 12:32:04 +0800

    Add Solidity icon via inline SVG in blockchain tech group

--- src/sections/techStack.js
+++ src/sections/techStack.js
@@ -73,6 +73,21 @@ import {
   FaCoins,
 } from 'react-icons/fa';
 
+function SolidityIcon() {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      role="img"
+      aria-label="Solidity">
+      <path d="M4.409 6.608L7.981.255l3.572 6.353H4.409zM8.411 0l3.569 6.348L15.552 0H8.411zm4.036 17.392l3.572 6.354 3.575-6.354h-7.147zm-.608-10.284h-7.43l3.715 6.605 3.715-6.605zm.428-.25h7.428L15.982.255l-3.715 6.603zM15.589 24l-3.569-6.349L8.448 24h7.141zm-3.856-6.858H4.306l3.712 6.603 3.715-6.603zm.428-.25h7.433l-3.718-6.605-3.715 6.605z" />
+    </svg>
+  );
+}
+
 const groups = [
   {
     id: 1,
@@ -151,6 +166,7 @@ const groups = [
     category: 'Blockchain & Web3',
     items: [
       { name: 'Substrate', icon: <SiParitysubstrate /> },
+      { name: 'Solidity', icon: <SolidityIcon /> },
       { name: 'Rust', icon: <SiRust /> },
       { name: 'IPFS', icon: <SiIpfs /> },
       { name: 'Smart Contracts', icon: <FaFileCode /> },
